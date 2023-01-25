4b9dc59b086f33383d341c0769631db8acfbd4ec
jungpa <operland55@naver.com>
2023-01-25 18:33:02 +0900

    ✨Banner Component and style

---
+++ src/Components/Banner.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Img from "../assets/koreaBg1.jpg";
+import Img2 from "../assets/koreaBg2.jpg";
+import Img3 from "../assets/koreaBg3.jpg";
+import illust from "../assets/illust.jpg";
+
+export const Banner = () => {
+	return (
+		<div className="banner-wrapper">
+			<div className="banner-main">
+				<div className="banner-info">
+					<div className="img-box one">
+						<img src={Img} alt="korea-Img" />
+					</div>
+					<p>
+						ようこそ <br />
+						韓国に来てくれて
+					</p>
+				</div>
+				<div className="banner-info">
+					<p>俺たちが思い出を作ってあげます</p>
+					<div className="img-box two">
+						<img src={Img2} alt="korea-Img" />
+					</div>
+				</div>
+				<div className="banner-info">
+					<div className="img-box three">
+						<img src={Img3} alt="korea-Img" />
+					</div>
+					<p>あなたは思い出を持って帰ってください。</p>
+				</div>
+
+				<div className="illust">
+					<img src={illust} alt="illust" />
+				</div>
+			</div>
+		</div>
+	);
+};
