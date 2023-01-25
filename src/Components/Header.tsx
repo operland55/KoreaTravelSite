3ab09762580519b2caa827a2bfa41b42f6044b79
jungpa <operland55@naver.com>
2023-01-25 18:33:26 +0900

    ✨Make Header Component and styles

---
+++ src/Components/Header.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { navObj } from "../obj";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { SideMenu } from "./SideMenu";
+export const Header = () => {
+	const [active, setActive] = useState("");
+	return (
+		<>
+			<div className="header-wrapper">
+				<header>
+					<h1>旅行</h1>
+					<AiOutlineMenu
+						className="sm-only"
+						onClick={() => {
+							setActive("active");
+						}}
+					/>
+					<ul className="md-only">
+						{navObj.map((item, key) => (
+							<li key={key}>
+								<p>{item.name}</p>
+							</li>
+						))}
+					</ul>
+				</header>
+			</div>
+			<SideMenu active={active} setActive={setActive} />
+		</>
+	);
+};
