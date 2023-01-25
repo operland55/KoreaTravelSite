eae37b2b8be292d55121ef507cff4513fd4e30d7
jungpa <operland55@naver.com>
2023-01-25 18:32:19 +0900

    ✨travel info obj

---
+++ src/obj.tsx
@@ -0,0 +1,22 @@
+export const navObj = [
+	{
+		name: "どんな旅行",
+		link: "about",
+	},
+	{
+		name: "どこへ旅する？",
+		link: "trip",
+	},
+	{
+		name: "体験レポート",
+		link: "report",
+	},
+	{
+		name: "よくある質問",
+		link: "faq",
+	},
+	{
+		name: "お問い合わせ",
+		link: "Contact",
+	},
+];
