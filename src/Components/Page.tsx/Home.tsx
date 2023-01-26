8b13aa1f5d99cb10d97a4fffe435db2819fde754
jungpa <operland55@naver.com>
2023-01-26 16:41:43 +0900

    ✅ Add concept component

--- src/Components/Page.tsx/Home.tsx
+++ src/Components/Page.tsx/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Banner } from "../Banner";
+import { Concept } from "../Concept";
 
 export const Home = () => {
 	return (
 		<>
 			<Banner />
+			<Concept />
 		</>
 	);
 };
