0b26cd6ce2b37a9fe6566b347025815a18eb5fb3
jungpa <operland55@naver.com>
2023-01-27 16:38:55 +0900

    ⏪Change Img

--- src/obj.tsx
+++ src/obj.tsx
@@ -1,3 +1,11 @@
+import img from "../src/assets/concept1.jpg";
+import tripimg from "../src/assets/tripimg.jpg";
+import tripimg2 from "../src/assets/tripimg2.jpg";
+import tripimg3 from "../src/assets/tripimg3.jpg";
+import tripimg4 from "../src/assets/tripimg4.jpg";
+import tripimg5 from "../src/assets/tripimg5.jpg";
+import tripimg6 from "../src/assets/tripimg6.jpg";
+
 export const navObj = [
 	{
 		name: "どんな旅行",
@@ -18,5 +26,50 @@ export const navObj = [
 	{
 		name: "お問い合わせ",
 		link: "Contact",
+	},
+];
+
+export const tripObj = [
+	{
+		img: tripimg,
+		name: "南山タワー",
+		info: "外国人なら必ず来るところ",
+		location: "龍山（ソウル）",
+		theme: "#カップル #背景 #ロック #観光地",
+	},
+	{
+		img: tripimg2,
+		name: "明洞",
+		info: "韓国の食べ物やものを買いたいならここに来てください",
+		location: "明洞(ソウル)",
+		theme: "#食品 #商品 #観光地",
+	},
+	{
+		img: tripimg3,
+		name: "ロッテワールド",
+		info: `大人になってもここでは制服を着て遊びます`,
+		location: "松坡区(ソウル)",
+		theme: "#乗り物, #制服",
+	},
+	{
+		img: tripimg4,
+		name: "韓屋村",
+		info: "日本の着物があれば韓国の韓服がある",
+		location: "ジョンロ(ソウル)",
+		theme: "#韓服 #観光地",
+	},
+	{
+		img: tripimg5,
+		name: "日本に最も近い都市は釜山",
+		info: "釜山はどこに行った観光地です！",
+		location: "釜山",
+		theme: "#食べ物 #観光地",
+	},
+	{
+		img: tripimg6,
+		name: "済州島",
+		info: "世界7大美しい都市に選ばれた島！",
+		location: "済州島",
+		theme: "#海 #きれい #メイド #観光地",
 	},
 ];
