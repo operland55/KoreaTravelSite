4dab516b147b3db1ebab33f2897418d9484b58bb
jungpa <operland55@naver.com>
2023-01-30 07:48:48 +0900

    ✅ Add reportobj

--- src/obj.tsx
+++ src/obj.tsx
@@ -6,7 +6,45 @@ import tripImg3 from "../src/assets/tripimg3.jpg";
 import tripImg4 from "../src/assets/tripimg4.jpg";
 import tripImg5 from "../src/assets/tripimg5.jpg";
 import tripImg6 from "../src/assets/tripimg6.jpg";
+import report from "../src/assets/report.jpg";
+import report2 from "../src/assets/report2.jpg";
+import report3 from "../src/assets/report3.jpg";
+import report4 from "../src/assets/report4.jpg";
+import logo from "../src/assets/logo남산.jpg";
+import logo2 from "../src/assets/logo명동.jpg";
+import logo3 from "../src/assets/logo부산.jpg";
+import logo4 from "../src/assets/logo제주.jpg";
 
+export const reportObj = [
+	{
+		img: report,
+		logo: logo,
+		name: "ナナ & ハル",
+		location: "ロッテワールド",
+		info: "久しぶりに友達と制服を着て遊園地に来て幸せでした。",
+	},
+	{
+		img: report2,
+		logo: logo2,
+		name: "ミハル & サナ",
+		location: "明洞",
+		info: "食べる場所とかわいいものが多すぎて見て忙しかったです。",
+	},
+	{
+		img: report3,
+		logo: logo3,
+		name: "モモ & ミナ",
+		location: "釜山",
+		info: "食べる場所もたくさんあり、特にここは家がおもちゃのようです。",
+	},
+	{
+		img: report4,
+		logo: logo4,
+		name: "アコ & ナカノメイ",
+		location: "済州島",
+		info: "海はとても透明で美しいです。ここで住みたいです。",
+	},
+];
 export const navObj = [
 	{
 		name: "どんな旅行",
